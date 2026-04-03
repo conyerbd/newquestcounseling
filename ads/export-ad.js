ca1222da85cc6a72c40655cfa623b275ed59b74d
Brad Conyers <conyerbd@gmail.com>
2026-04-03 19:24:54 -0400

    contact pixel

--- ads/export-ad.js
+++ ads/export-ad.js
@@ -26,5 +26,25 @@ const path = require('path');
     });
 
     console.log('Exported ad1.png (1080x1080)');
+
+    // Export vertical version (1080x1920) for Stories/Reels
+    await page.setViewport({ width: 1080, height: 1920, deviceScaleFactor: 1 });
+
+    const verticalPath = path.resolve(__dirname, 'ad1-vertical.html');
+    await page.goto(`file://${verticalPath}`, { waitUntil: 'networkidle0' });
+
+    await page.evaluate(() => {
+        document.body.style.padding = '0';
+        document.body.style.margin = '0';
+        document.body.style.minHeight = '1920px';
+    });
+
+    await page.screenshot({
+        path: path.resolve(__dirname, 'ad1-vertical.png'),
+        type: 'png',
+        clip: { x: 0, y: 0, width: 1080, height: 1920 },
+    });
+
+    console.log('Exported ad1-vertical.png (1080x1920)');
     await browser.close();
 })();
